e0e59968e06fd844c474536013ffb481c4d42dcf
MKhamatshina <maria.khamat@mail.ru>
2023-04-13 07:56:37 +0300

    task 4 adjusted

--- task_4/solution.js
+++ task_4/solution.js
@@ -6,8 +6,8 @@ function parseProducts(json) {
 }
     function renderProductsCards(json){
         clearProducts();
-        products = parseProducts(json);
-        length = products.length;
+        let products = parseProducts(json);
+        let length = products.length;
         for (let i = 0; i < length; i += 1) {
             addProduct(products[i]);
          }
